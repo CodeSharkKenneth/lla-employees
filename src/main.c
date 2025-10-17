fcd690236e95bb06d1c217f51b8e464c4b3034bc
Kenneth Clark <kenneth@codeshark.co.za>
2025-10-17 17:35:03 +0200

    "Implement database file creation, validation, and header management; add utility functions and initial structures."

--- src/main.c
+++ src/main.c
@@ -19,6 +19,8 @@ int main(int argc, char *argv[]) {
     bool newfile = false;
     int c;
 
+    int dbfd = -1;
+    struct dbheader_t *header = NULL;
 
     while((c = getopt(argc, argv, "nf:")) != -1){
         switch(c){
@@ -42,8 +44,31 @@ int main(int argc, char *argv[]) {
 		return 0;
 	}
 
-	printf("Newfile: %d\n", newfile);
-	printf("Filepath: %s\n", filepath);
+    if(newfile){
+        dbfd = create_db_file(filepath);
+        if(dbfd == STATUS_ERROR){
+            printf("Unable to create database file\n");
+            return -1;
+        }
+
+        if(create_db_header(dbfd, &header) == STATUS_ERROR){
+            printf("Unable to create database header\n");
+            return -1;
+        }
+    }else{
+        dbfd = open_db_file(filepath);
+        if(dbfd == STATUS_ERROR){
+            printf("Unable to open database file\n");
+            return -1;
+        }
+
+        if(validate_db_header(dbfd, &header) == STATUS_ERROR){
+            printf("Unable to validate database header\n");
+            return -1;
+        }
+    }
+
+    output_file(dbfd, header);
 
     return 0;
 }
