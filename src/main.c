4d15192dbf375354dc08cf1d2ebafbbfaa22bdbf
Kenneth Clark <kenneth@codeshark.co.za>
2025-10-18 12:13:28 +0200

    "Add support for listing and adding employees via CLI; update related parsing, file handling, and utility functions."

--- src/main.c
+++ src/main.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include <stdbool.h>
 #include <getopt.h>
 
@@ -10,25 +11,32 @@ void print_usage(char *argv[]){
 	printf("Usage: %s -n -f <database file>\n", argv[0]);
 	printf("\t -n  - create new database file\n");
 	printf("\t -f  - (required) path to database file\n");
+	printf("\t -l  - list the employees\n");
+	printf("\t -a  - add via CSV list of (name,address,hours)\n");
 	return;
 }
 
 int main(int argc, char *argv[]) {
 
     char *filepath = NULL;
+    char *addstring = NULL;
     bool newfile = false;
     int c;
 
     int dbfd = -1;
     struct dbheader_t *header = NULL;
+    struct employee_t *employees = NULL;
 
-    while((c = getopt(argc, argv, "nf:")) != -1){
+    while((c = getopt(argc, argv, "nf:a:")) != -1){
         switch(c){
             case 'n':
                 newfile = true;
                 break;
             case 'f':
                 filepath = optarg;
+                break;
+            case 'a':
+                addstring = optarg;
                 break;
 			case '?':
 				printf("Unrecognized option '%c'\n", optopt);
@@ -68,7 +76,16 @@ int main(int argc, char *argv[]) {
         }
     }
 
-    output_file(dbfd, header);
+    if(read_employees(dbfd, header, &employees) != STATUS_SUCCESS){
+        printf("Unable to read employees\n");
+        return -1;
+    }
+
+    if(addstring != NULL){
+        add_employee(header, &employees, addstring);
+    }
+
+    output_file(dbfd, header, employees);
 
     return 0;
 }
--- src/parse.c
+++ src/parse.c
@@ -13,6 +13,41 @@
 #include "common.h"
 #include "parse.h"
 
+int add_employee(struct dbheader_t *header, struct employee_t **employees, char *addstring){
+
+	if(NULL == header) return STATUS_ERROR;
+	if(NULL == employees) return STATUS_ERROR;
+	if(NULL == *employees) return STATUS_ERROR;
+	if(NULL == addstring) return STATUS_ERROR;
+
+    char *name = strtok(addstring, ",");
+	if(NULL == name) return STATUS_ERROR;
+
+    char *address = strtok(NULL, ",");
+	if(NULL == address) return STATUS_ERROR;
+
+    char *hours = strtok(NULL, ",");
+	if(NULL == hours) return STATUS_ERROR;
+
+    struct employee_t *e = *employees;
+    e = realloc(e, sizeof(struct employee_t) * (header->count + 1));
+
+    if(e == NULL){
+        printf("Realloc failed to add employee\n");
+        return STATUS_ERROR;
+    }
+
+	header->count++;
+
+    strncpy(e[header->count - 1].name, name, sizeof(e[header->count-1].name-1));
+    strncpy(e[header->count - 1].address, address, sizeof(e[header->count-1].address-1));
+    e[header->count - 1].hours = atoi(hours);
+
+	*employees = e;
+
+    return STATUS_SUCCESS;
+}
+
 int create_db_header(struct dbheader_t **headerOut){
     struct dbheader_t *header = calloc(1, sizeof(struct dbheader_t));
 
@@ -33,7 +68,7 @@ int create_db_header(struct dbheader_t **headerOut){
 
 int validate_db_header(int fd, struct dbheader_t **headerOut){
     if(fd < 0){
-        printf("Go a bad FD from the user\n");
+        printf("Got a bad FD from the user\n");
         return STATUS_ERROR;
     }
 
@@ -78,18 +113,55 @@ int validate_db_header(int fd, struct dbheader_t **headerOut){
     return STATUS_SUCCESS;
 }
 
-void output_file(int fd, struct dbheader_t *dbhdr){
+void output_file(int fd, struct dbheader_t *dbhdr, struct employee_t *employees){
     if(fd < 0){
-        printf("Go a bad FD from the user\n");
+        printf("Got a bad FD from the user\n");
         return;
     }
 
+    int header_count = dbhdr->count;
+
     dbhdr->magic = htonl(dbhdr->magic);
-    dbhdr->filesize = htonl(dbhdr->filesize);
+    dbhdr->filesize = htonl(
+        sizeof(struct dbheader_t) + header_count * sizeof(struct employee_t)
+    );
     dbhdr->count = htons(dbhdr->count);
     dbhdr->version = htons(dbhdr->version);
 
     lseek(fd, 0, SEEK_SET);
     write(fd, dbhdr, sizeof(struct dbheader_t));
+
+    int i = 0;
+
+    for(; i < header_count; i++){
+        employees[i].hours = htonl(employees[i].hours);
+        write(fd, &employees[i], sizeof(struct employee_t));
+    }
+
     return;
+}
+
+int read_employees(int fd, struct dbheader_t *header, struct employee_t **employeesOut){
+    if(fd < 0){
+        printf("Got a bad FD from the user\n");
+        return STATUS_ERROR;
+    }
+
+    int count = header->count;
+    struct employee_t *employees = calloc(count, sizeof(struct employee_t));
+
+    if(employees == NULL){
+        printf("Calloc failed to create employee array\n");
+        return STATUS_ERROR;
+    }
+
+    read(fd, employees, count * sizeof(struct employee_t));
+
+    int i = 0;
+    for(; i < count; i++){
+        employees[i].hours = ntohl(employees[i].hours);
+    }
+
+    *employeesOut = employees;
+    return STATUS_SUCCESS;
 }
